d13b80345f3621aefd369b63305e20ecd8bccbc1
Jack Arnold <jackarnold84@gmail.com>
2022-10-11 09:02:09 -0500

    week 5 update

--- run.py
+++ run.py
@@ -3,17 +3,17 @@ from signal import NSIG
 import builder
 
 
-builder.Builder('purdue-2022', week=4, n_sim=20000).build_report()
+builder.Builder('purdue-2022', week=5, n_sim=20000).build_report()
 
-builder.Builder('morton-2022', week=4, n_sim=10000).build_report()
+builder.Builder('morton-2022', week=5, n_sim=10000).build_report()
 
-builder.Builder('capitalone-2022', week=4, n_sim=10000).build_report()
+builder.Builder('capitalone-2022', week=5, n_sim=10000).build_report()
 
-builder.Builder('purdue-baseball-2022', week=16, n_sim=20000).build_report()
+# builder.Builder('purdue-baseball-2022', week=16, n_sim=20000).build_report()
 
-builder.Builder('purdue-2021', week=14, n_sim=20000).build_report()
+# builder.Builder('purdue-2021', week=14, n_sim=20000).build_report()
 
-builder.Builder('morton-2021', week=11, n_sim=20000).build_report()
+# builder.Builder('morton-2021', week=11, n_sim=20000).build_report()
 
 
 builder.build_homepage()
