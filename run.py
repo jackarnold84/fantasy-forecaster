93051e826847493872a72c1aacac2142f42f577f
Jack Arnold <jackarnold84@gmail.com>
2022-12-13 09:41:33 -0600

    week 14 update

--- run.py
+++ run.py
@@ -1,11 +1,11 @@
 import builder
 
 
-builder.Builder('purdue-2022', week=13, n_sim=30000).build_report()
+builder.Builder('purdue-2022', week=14, n_sim=30000).build_report()
 
-builder.Builder('morton-2022', week=13, n_sim=20000).build_report()
+builder.Builder('morton-2022', week=14, n_sim=20000).build_report()
 
-builder.Builder('capitalone-2022', week=13, n_sim=10000).build_report()
+builder.Builder('capitalone-2022', week=14, n_sim=10000).build_report()
 
 # builder.Builder('purdue-baseball-2022', week=16, n_sim=20000).build_report()
 
