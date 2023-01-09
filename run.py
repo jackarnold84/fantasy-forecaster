435bb3c29bd60f9d811397df005a59436bc866f4
Jack Arnold <jackarnold84@gmail.com>
2023-01-09 15:55:12 -0600

    week 18 data update

--- run.py
+++ run.py
@@ -2,8 +2,8 @@ from build.builder import Builder, build_homepage
 import metrics.players.evaluator as Player
 import metrics.league.evaluator as League
 
-p = Player.Evaluator(year=2022, week=17)
-l = League.Evaluator('purdue-2022', week=17, players=p.players, draft_recap_week=13)
+p = Player.Evaluator(year=2022, week=18)
+l = League.Evaluator('purdue-2022', week=18, players=p.players, draft_recap_week=13)
 
 Builder('purdue-2022', week=14, n_sim=300).build_report()
 
