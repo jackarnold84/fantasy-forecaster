348948f51b1e068a433a4644a304fc57d0506f79
Jack Arnold <jackarnold84@gmail.com>
2023-09-04 23:34:45 -0500

    add purdue football 2023

--- model/config.py
+++ model/config.py
@@ -1,4 +1,25 @@
 leagues = {
+    'football-2023': {
+        'purdue': {
+            'name': 'Practice Squad',
+            'league_tag': 'purdue',
+            'sport_tag': 'football-2023',
+            'league_id': '410886999',
+            'player_metrics': True,
+            'teams': 10,
+            'playoff_teams': 4,
+            'divisions': False,
+            'weeks_per_playoff_matchup': 2,
+            'total_weeks': 18,
+            'regular_season_weeks': 14,
+            'n_iter': 20000,
+            'model_params': {
+                'score_mean': 122,
+                'score_sd': 24,
+                'team_sd': 9,
+            }
+        }
+    },
     'baseball-2023': {
         'purdue': {
             'name': 'Practice Squad',
