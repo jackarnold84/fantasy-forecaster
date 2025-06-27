a114029ca2c6fd98083f657288158a1b785fd68b
jackarnold84 <jackarnold84@gmail.com>
2025-06-27 15:19:36 -0500

    Updates

--- component---src-pages-league-js-424a3cfafe76cbcfae70.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfantasy_forecaster=self.webpackChunkfantasy_forecaster||[]).push([[223],{7726:function(e,t,n){n.r(t),n.d(t,{Head:function(){return c}});var a=n(7294),r=(n(1728),n(3354),n(429),n(2437));t.default=()=>{const e=new Proxy(new URLSearchParams(window.location.search),{get:(e,t)=>e.get(t)});return console.log(e.id),a.createElement(r.Z,null,"Hello ",e.id)};const c=()=>a.createElement("title",null,"Fantasy Forecaster")}}]);
-//# sourceMappingURL=component---src-pages-league-js-424a3cfafe76cbcfae70.js.map
--- component---src-pages-league-js-54450457a44f9954fa83.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkfantasy_forecaster=self.webpackChunkfantasy_forecaster||[]).push([[223],{7726:function(e,t,a){a.r(t),a.d(t,{Head:function(){return c}});var n=a(7294),r=(a(1728),a(3354),a(429),a(2437));t.default=e=>{let{location:t}=e;const a=new Proxy(new URLSearchParams(t.search),{get:(e,t)=>e.get(t)});return console.log(a.id),n.createElement(r.Z,null,"Hello ",a.id)};const c=()=>n.createElement("title",null,"Fantasy Forecaster")}}]);
-//# sourceMappingURL=component---src-pages-league-js-54450457a44f9954fa83.js.map
